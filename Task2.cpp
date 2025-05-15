242a7af948731c27403f37d6f0b65318e01640ba
Suleman-Arshad <sulemanarshad0682@gmail.com>
2025-05-15 16:23:03 +0500

    Remove comment

--- Task2.cpp
+++ Task2.cpp
@@ -1,89 +1,89 @@
-//#include <iostream>
-//#include<stdlib.h>
-//using namespace std;
-//template <typename T>
-//class Duplicate
-//{
-//private:
-//    T* duplicates;
-//    int size;
-//
-//public:
-//    Duplicate();
-//    ~Duplicate();
-//    void input_array();
-//    void remove_duplicate();
-//    void display();
-//};
-//template <typename T>
-//Duplicate<T>::Duplicate() : size(0), duplicates(nullptr) {}
-//template <typename T>
-//void Duplicate<T>::input_array()
-//{
-//    cout << "Enter Size of an array: ";
-//    cin >> size;
-//    duplicates = new T[size];
-//    cout << "=========================Enter Elements of an array======================\n";
-//    for (int i = 0; i < size; i++)
-//    {
-//        cin >> duplicates[i];
-//    }
-//}
-//template <typename T>
-//void Duplicate<T>::remove_duplicate()
-//{
-//    for (int i = 0; i < size; i++)
-//    {
-//        for (int j = i + 1; j < size;)
-//        {
-//            if (duplicates[i] == duplicates[j])
-//            {
-//                for (int k = j; k < size - 1; k++)
-//                {
-//                    duplicates[k] = duplicates[k + 1];
-//                }
-//                size--;
-//            }
-//            else
-//            {
-//                j++;
-//            }
-//        }
-//    }
-//}
-//template <typename T>
-//void Duplicate<T>::display()
-//{
-//    cout << "=========================Array without Duplicates======================\n";
-//    for (int i = 0; i < size; i++)
-//    {
-//        cout << duplicates[i] << "\t";
-//    }
-//    cout << endl;
-//}
-//template <typename T>
-//Duplicate<T>::~Duplicate()
-//{
-//    delete[] duplicates;
-//    duplicates = nullptr;
-//}
-//int main()
-//{
-//    cout << "\n======================Integer array=================\n";
-//    Duplicate<int> obj;
-//    obj.input_array();
-//    obj.remove_duplicate();
-//    obj.display();
-//    cout << "\n======================Float array=================\n";
-//    Duplicate<float> floatObj;
-//    floatObj.input_array();
-//    floatObj.remove_duplicate();
-//    floatObj.display();
-//    cout << "\n======================Character array=================\n";
-//    Duplicate<char> charObj;
-//    charObj.input_array();
-//    charObj.remove_duplicate();
-//    charObj.display();
-//    system("pause");
-//    return 0;
-//}
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+template <typename T>
+class Duplicate
+{
+private:
+    T* duplicates;
+    int size;
+
+public:
+    Duplicate();
+    ~Duplicate();
+    void input_array();
+    void remove_duplicate();
+    void display();
+};
+template <typename T>
+Duplicate<T>::Duplicate() : size(0), duplicates(nullptr) {}
+template <typename T>
+void Duplicate<T>::input_array()
+{
+    cout << "Enter Size of an array: ";
+    cin >> size;
+    duplicates = new T[size];
+    cout << "=========================Enter Elements of an array======================\n";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> duplicates[i];
+    }
+}
+template <typename T>
+void Duplicate<T>::remove_duplicate()
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size;)
+        {
+            if (duplicates[i] == duplicates[j])
+            {
+                for (int k = j; k < size - 1; k++)
+                {
+                    duplicates[k] = duplicates[k + 1];
+                }
+                size--;
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+}
+template <typename T>
+void Duplicate<T>::display()
+{
+    cout << "=========================Array without Duplicates======================\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << duplicates[i] << "\t";
+    }
+    cout << endl;
+}
+template <typename T>
+Duplicate<T>::~Duplicate()
+{
+    delete[] duplicates;
+    duplicates = nullptr;
+}
+int main()
+{
+    cout << "\n======================Integer array=================\n";
+    Duplicate<int> obj;
+    obj.input_array();
+    obj.remove_duplicate();
+    obj.display();
+    cout << "\n======================Float array=================\n";
+    Duplicate<float> floatObj;
+    floatObj.input_array();
+    floatObj.remove_duplicate();
+    floatObj.display();
+    cout << "\n======================Character array=================\n";
+    Duplicate<char> charObj;
+    charObj.input_array();
+    charObj.remove_duplicate();
+    charObj.display();
+    system("pause");
+    return 0;
+}
--- Task5.cpp
+++ Task5.cpp
@@ -1,72 +1,72 @@
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//class invalidHr
-//{
-//};
-//class invalidMin
-//{
-//};
-//class invalidSec
-//{
-//};
-//int main()
-//{
-//    int hour, minute, second;
-//    string am_pm;
-//    try
-//    {
-//        cout << "Enter time in 12-hour format (hh mm ss AM/PM): ";
-//        cin >> hour >> minute >> second >> am_pm;
-//        for (int i = 0; i < am_pm.length(); i++)
-//        {
-//            if (am_pm[i] >= 'a' && am_pm[i] <= 'z')
-//            {
-//                am_pm[i] = am_pm[i] - 32;
-//            }
-//        }
-//        if (hour < 1 || hour > 12)
-//        {
-//            throw invalidHr();
-//        }
-//        if (minute < 0 || minute > 59)
-//        {
-//            throw invalidMin();
-//        }
-//        if (second < 0 || second > 59)
-//        {
-//            throw invalidSec();
-//        }
-//        if (am_pm == "AM")
-//        {
-//            if (hour == 12)
-//                hour = 0;
-//        }
-//        else if (am_pm == "PM")
-//        {
-//            if (hour != 12)
-//                hour += 12;
-//        }
-//        else
-//        {
-//            cout << "Error: Invalid AM/PM format." << endl;
-//            return 1;
-//        }
-//
-//        cout << "24-hour format: " << (hour < 10 ? "0" : "") << hour << ":" << (minute < 10 ? "0" : "") << minute << ":" << (second < 10 ? "0" : "") << second << endl;
-//    }
-//    catch (invalidHr)
-//    {
-//        cout << "Error: Invalid hour. Must be between 1 and 12." << endl;
-//    }
-//    catch (invalidMin)
-//    {
-//        cout << "Error: Invalid minute. Must be between 0 and 59." << endl;
-//    }
-//    catch (invalidSec)
-//    {
-//        cout << "Error: Invalid second. Must be between 0 and 59." << endl;
-//    }
-//    system("pause");
-//    return 0;
-//}
+#include <iostream>
+#include <string>
+using namespace std;
+class invalidHr
+{
+};
+class invalidMin
+{
+};
+class invalidSec
+{
+};
+int main()
+{
+    int hour, minute, second;
+    string am_pm;
+    try
+    {
+        cout << "Enter time in 12-hour format (hh mm ss AM/PM): ";
+        cin >> hour >> minute >> second >> am_pm;
+        for (int i = 0; i < am_pm.length(); i++)
+        {
+            if (am_pm[i] >= 'a' && am_pm[i] <= 'z')
+            {
+                am_pm[i] = am_pm[i] - 32;
+            }
+        }
+        if (hour < 1 || hour > 12)
+        {
+            throw invalidHr();
+        }
+        if (minute < 0 || minute > 59)
+        {
+            throw invalidMin();
+        }
+        if (second < 0 || second > 59)
+        {
+            throw invalidSec();
+        }
+        if (am_pm == "AM")
+        {
+            if (hour == 12)
+                hour = 0;
+        }
+        else if (am_pm == "PM")
+        {
+            if (hour != 12)
+                hour += 12;
+        }
+        else
+        {
+            cout << "Error: Invalid AM/PM format." << endl;
+            return 1;
+        }
+
+        cout << "24-hour format: " << (hour < 10 ? "0" : "") << hour << ":" << (minute < 10 ? "0" : "") << minute << ":" << (second < 10 ? "0" : "") << second << endl;
+    }
+    catch (invalidHr)
+    {
+        cout << "Error: Invalid hour. Must be between 1 and 12." << endl;
+    }
+    catch (invalidMin)
+    {
+        cout << "Error: Invalid minute. Must be between 0 and 59." << endl;
+    }
+    catch (invalidSec)
+    {
+        cout << "Error: Invalid second. Must be between 0 and 59." << endl;
+    }
+    system("pause");
+    return 0;
+}
